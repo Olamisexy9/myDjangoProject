f2dfb25b4c7f9c71c5056cd809ff25dc37f3d3a7
Olamisexy9 <tijaniolamilekan@gmail.com>
2022-10-26 21:48:05 +0100

    models

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Artiste (models.Model):
+    artiste_id = models.CharField(max_Lenght=20)
+    first_name = models.CharField(max_Lenght=50)
+    last_name = models.CharField(max_Lenght=50)
+    age = models.CharField(max_Lenght=20)
+
+
+class Song (models.Model):
+    Artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    song_id = models.CharField(max_Lenght=20)
+    song_title = models.CharField(max_Lenght=400)
+    date_released = models.CharField(max_Lenght=400)
+    likes =models.CharField(max_Lenght=400)
+    artiste_id = models.CharField(max_Lenght=400)
+
+class Lyric (models.Model):
+    Song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    lyric_id = models.CharField(max_Lenght=20)
+    content = models.CharField(max_Lenght=400)
+    song_id = models.CharField(max_Lenght=400)
